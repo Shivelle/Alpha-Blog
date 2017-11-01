9d8ffb4401d50b775d09032f95db808db4003492
Sarah Maria Leidisch <shivelle@gmx.net>
2017-11-01 11:43:40 +0000

    fix controller bugs resulting in delete route error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,10 @@
 class ArticlesController < ApplicationController
-  
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def index
-    @articles = Article.paginate(page: params[:page], per_page:5)
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
   
   def new
@@ -15,9 +16,9 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
-      flash[:success] = "Aricle was successfully created."
+      flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -26,9 +27,9 @@ class ArticlesController < ApplicationController
   
   def update
     if @article.update(article_params)
-      flash[:success] = "Article was successfully updated."
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
-    else 
+    else
       render 'edit'
     end
   end
@@ -38,17 +39,24 @@ class ArticlesController < ApplicationController
   
   def destroy
     @article.destroy
-    flash[:danger] = "Article was successfully destroyed."
+    flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
   
   private
     def set_article
-      @article = Article.find(params[:id])
+      @article = Article.find(params[:id])  
     end
-    
+  
     def article_params
-        params.require(:article).permit(:title, :description)
+      params.require(:article).permit(:title, :description, category_ids: [])
+    end
+  
+    def require_same_user
+      if current_user != @article.user and !current_user.admin?
+        flash[:danger] = "You can only edit or delete your own articles"
+        redirect_to root_path
+      end
     end
   
 end
